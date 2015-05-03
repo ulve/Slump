205e1d5620d766f0728d52033cfe25f424de28ca
Olov Johansson <johansson.olov@gmail.com>
2015-05-03 13:14:46 +0200

    Initial import
    Added project

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Slump
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSlumpa = new System.Windows.Forms.Button();
+            this.btnSpecifik = new System.Windows.Forms.Button();
+            this.txtRadnummer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnÖppnaFil = new System.Windows.Forms.Button();
+            this.txtSökväg = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstTidigareSlag = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSlumpa
+            // 
+            this.btnSlumpa.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSlumpa.Location = new System.Drawing.Point(371, 78);
+            this.btnSlumpa.Name = "btnSlumpa";
+            this.btnSlumpa.Size = new System.Drawing.Size(75, 23);
+            this.btnSlumpa.TabIndex = 0;
+            this.btnSlumpa.Text = "Slumpa";
+            this.btnSlumpa.UseVisualStyleBackColor = true;
+            this.btnSlumpa.Click += new System.EventHandler(this.btnSlumpa_Click);
+            // 
+            // btnSpecifik
+            // 
+            this.btnSpecifik.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSpecifik.Location = new System.Drawing.Point(290, 78);
+            this.btnSpecifik.Name = "btnSpecifik";
+            this.btnSpecifik.Size = new System.Drawing.Size(75, 23);
+            this.btnSpecifik.TabIndex = 1;
+            this.btnSpecifik.Text = "Gå till";
+            this.btnSpecifik.UseVisualStyleBackColor = true;
+            this.btnSpecifik.Click += new System.EventHandler(this.btnSpecifik_Click);
+            // 
+            // txtRadnummer
+            // 
+            this.txtRadnummer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRadnummer.Location = new System.Drawing.Point(12, 81);
+            this.txtRadnummer.Name = "txtRadnummer";
+            this.txtRadnummer.Size = new System.Drawing.Size(272, 20);
+            this.txtRadnummer.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnÖppnaFil
+            // 
+            this.btnÖppnaFil.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnÖppnaFil.Location = new System.Drawing.Point(369, 16);
+            this.btnÖppnaFil.Name = "btnÖppnaFil";
+            this.btnÖppnaFil.Size = new System.Drawing.Size(75, 23);
+            this.btnÖppnaFil.TabIndex = 4;
+            this.btnÖppnaFil.Text = "Öppna fil";
+            this.btnÖppnaFil.UseVisualStyleBackColor = true;
+            this.btnÖppnaFil.Click += new System.EventHandler(this.btnÖppnaFil_Click);
+            // 
+            // txtSökväg
+            // 
+            this.txtSökväg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSökväg.Location = new System.Drawing.Point(12, 19);
+            this.txtSökväg.Name = "txtSökväg";
+            this.txtSökväg.ReadOnly = true;
+            this.txtSökväg.Size = new System.Drawing.Size(351, 20);
+            this.txtSökväg.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Rad nummer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Resultat";
+            // 
+            // lstTidigareSlag
+            // 
+            this.lstTidigareSlag.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstTidigareSlag.FormattingEnabled = true;
+            this.lstTidigareSlag.Location = new System.Drawing.Point(12, 121);
+            this.lstTidigareSlag.Name = "lstTidigareSlag";
+            this.lstTidigareSlag.Size = new System.Drawing.Size(432, 199);
+            this.lstTidigareSlag.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 338);
+            this.Controls.Add(this.lstTidigareSlag);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSökväg);
+            this.Controls.Add(this.btnÖppnaFil);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRadnummer);
+            this.Controls.Add(this.btnSpecifik);
+            this.Controls.Add(this.btnSlumpa);
+            this.Name = "Form1";
+            this.Text = "Slumpprogram";            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSlumpa;
+        private System.Windows.Forms.Button btnSpecifik;
+        private System.Windows.Forms.TextBox txtRadnummer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnÖppnaFil;
+        private System.Windows.Forms.TextBox txtSökväg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstTidigareSlag;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slump
+{
+    public partial class Form1 : Form
+    {
+        List<string> Rader { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            var fil = HämtaFörstaTxtFilen();
+            ÖppnaNyFil(fil);
+        }
+
+        private void ÖppnaNyFil(string fil)
+        {            
+            if (fil != null)
+            {
+                txtSökväg.Text = Path.Combine(Directory.GetCurrentDirectory(), fil);
+                LaddaFil(fil);
+                UppdateraAntal();
+            }
+        }
+
+        private void UppdateraAntal()
+        {
+            label1.Text = Rader.Count + " rader laddades";
+        }
+
+        private void LaddaFil(string fil)
+        {
+            Rader = File.ReadAllLines(fil).ToList();
+        }
+
+        private string HämtaFörstaTxtFilen()
+        {
+            DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory());
+            return di.GetFiles("*.txt")
+                .Select(fi => fi.Name)
+                .FirstOrDefault();
+        }
+
+        private void btnSlumpa_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            var valdRad = Rader[r.Next(Rader.Count)];
+            lstTidigareSlag.Items.Insert(0, ParsaRad(valdRad));            
+        }
+
+        private void btnÖppnaFil_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {                
+                ÖppnaNyFil(openFileDialog1.FileName);
+            }
+        }        
+
+        private void btnSpecifik_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var valdRad = Rader[Convert.ToInt32(txtRadnummer.Text) - 1];
+                lstTidigareSlag.Items.Insert(0, ParsaRad(valdRad));
+            }
+            catch
+            {
+                MessageBox.Show("Ange ett värde mellan 1 och "+ Rader.Count());
+            }
+        }
+
+        private string ParsaRad(string valdRad)
+        {
+            string pattern = @"(\d+)[td](\d+)([+-]\d+)*";
+            var reg = new Regex(pattern, RegexOptions.IgnoreCase);
+            var match = reg.Match(valdRad);
+            if(match.Success)
+            {
+                var antal =  Convert.ToInt32(match.Groups[1].Value);
+                var tärning =  Convert.ToInt32(match.Groups[2].Value);
+                var modifierare =  TillSiffra(match.Groups[3].Value);
+
+                var resultat = SlåTärning(antal, tärning, modifierare);
+                return reg.Replace(valdRad, resultat).Replace("[", "").Replace("]", "");
+            }
+            return valdRad;
+        }
+
+
+        private int TillSiffra(string modifierare)
+        {
+            if (string.IsNullOrEmpty(modifierare))
+                return 0;
+
+            bool negativ = false;
+            if(modifierare.Contains("-"))
+                negativ = true;
+            
+            modifierare = modifierare.Replace("+", "").Replace("-", "");
+            var siffra = Convert.ToInt32(modifierare);
+            if(negativ)
+                siffra = siffra * - 1;
+
+            return siffra;            
+        }
+
+        private string SlåTärning(int antal, int tärning, int modifierare)
+        {
+            var resultat = modifierare;
+            for(int i = 0; i < antal; i++)
+            {
+                Random r = new Random();
+                resultat += r.Next(1, tärning);
+            }
+            return resultat.ToString();
+        }        
+    }
+}
